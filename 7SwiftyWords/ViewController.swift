d2d651a6f0da6640b45dfffedb8e54c8965ea612
marianneji <marianneji@gmail.com>
2019-07-25 13:10:03 +0200

    Initial Commit

---
+++ 7SwiftyWords/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  7SwiftyWords
+//
+//  Created by Graphic Influence on 25/07/2019.
+//  Copyright © 2019 marianne massé. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
